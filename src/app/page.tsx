d4f8a127408a836171c5a453363f99540c211092
Dante Lentsoe <dante@nml.co.za>
2024-04-01 22:00:55 +0200

    feat: add FAQs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,35 @@
+import { appInfo } from '@/contants';
 import { SparklesPreview } from './components/Templates/SparklesPreview';
+import { InfoCard } from './components/UI/info-card';
+import { FAQItem, FAQItemProps } from './components/molecules/FAQItem';
+const FAQs: Array<FAQItemProps> = [
+  {
+    question: 'How It Works',
+    answer:
+      "Start by uploading your Excel file to Sheetwise. Use our dynamic table to view, edit, and manage your data with ease. Once you're done, easily save your changes back to your Excel file.",
+  },
+  {
+    question: 'Can I integrate Google Sheets?',
+    answer:
+      'Yes, you can integrate your Google Sheets and manage your data directly within Sheetwise. Any changes you make are instantly reflected in your Google Sheets, keeping everything up-to-date.',
+  },
+  {
+    question: 'Is my data secure?',
+    answer:
+      'Your data stays on your device, with no need to send it to a server. You have complete control over your data without worrying about privacy or security concerns.',
+  },
+];
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <SparklesPreview />
+      <div className="max-w-5xl mx-auto lg:px-8">
+        {FAQs.map(({ answer, question }) => {
+          return <FAQItem key={question} question={question} answer={answer} />;
+        })}
+        <InfoCard items={appInfo} />
+      </div>
     </main>
   );
 }
