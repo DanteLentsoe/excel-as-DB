a457727e542b195a6ab544b51bc137613225c6d6
Dante Lentsoe <dante@nml.co.za>
2024-04-09 22:01:43 +0200

    chore: add config properties

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  env: {
+    EMAIL_JS_TEMPLATE_ID: process.env.EMAIL_JS_TEMPLATE_ID,
+    EMAIL_JS_SERVICE_ID: process.env.EMAIL_JS_SERVICE_ID,
+    EMAIL_JS_USER_ID: process.env.EMAIL_JS_USER_ID,
+  },
+};
 
 export default nextConfig;
