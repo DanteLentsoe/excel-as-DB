8a4c57e01f3502a91015aaff12c8cca2012de398
Dante Lentsoe <dante@nml.co.za>
2024-04-09 07:50:44 +0200

    ref: update footer link to nextjs component

--- src/app/components/molecules/Footer/index.tsx
+++ src/app/components/molecules/Footer/index.tsx
@@ -30,7 +30,7 @@ export const Footer = () => {
               </Link>
             </li>
             <li>
-              <Link href="#" className="hover:underline">
+              <Link href="/contact" className="hover:underline">
                 Contact
               </Link>
             </li>
@@ -39,9 +39,12 @@ export const Footer = () => {
         <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
         <span className="block text-sm text-gray-500 sm:text-center dark:text-gray-400">
           © {date} made with ❤️ by{' '}
-          <a href="https://github.com/dantelentsoe" className="hover:underline">
+          <Link
+            href="https://github.com/dantelentsoe"
+            className="hover:underline"
+          >
             Dante Lentsoe
-          </a>
+          </Link>
           . All Rights Reserved.
         </span>
       </div>
